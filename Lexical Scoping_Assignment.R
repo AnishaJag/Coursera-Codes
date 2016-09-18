ab470677338561bbf180df829f1a7671946aa9c8
AnishaJag <ani.jagadesan@gmail.com>
2016-09-18 20:08:18 +0530

    Add files via upload

---
+++ Lexical Scoping_Assignment.R
@@ -0,0 +1,27 @@
+makematrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv= setinv,
+       getinv = getinv)
+}
+
+
+cacheinv <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- matrix1$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
+}
+
